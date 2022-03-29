e56420deb07ea2e733aa40771b67f3125d0f18b6
joycesaquino <joycesaquino@gmail.com>
2022-03-28 23:52:43 -0300

    Implementing wallet model

---
+++ src/main/java/com/meli/wallet/model/Auditable.java
@@ -0,0 +1,7 @@
+package com.meli.wallet.model;
+
+public interface Auditable {
+
+    Audit getAuditable();
+    void setAuditable(Audit auditable);
+}
--- src/main/java/com/meli/wallet/model/wallet/Wallet.java
+++ src/main/java/com/meli/wallet/model/wallet/Wallet.java
@@ -1,18 +1,29 @@
 package com.meli.wallet.model.wallet;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.meli.wallet.enums.Type;
+import com.meli.wallet.model.Audit;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
 
-@Entity
-@Getter
-@Setter
-@Table(name = "wallet")
-public class Wallet {
+@Data
+@DynamoDBTable(tableName = "wallet")
+public class Wallet extends Audit {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @DynamoDBRangeKey
     private Long id;
+
+    @DynamoDBHashKey
+    private Long userId;
+    private UUID traceId;
+    private Integer quantity;
+    private BigDecimal value;
 
 }
