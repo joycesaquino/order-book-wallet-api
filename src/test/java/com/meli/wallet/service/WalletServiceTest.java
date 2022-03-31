00f7c8a99ad4f01b985072f6788f73380c6ebfce
joycesaquino <joycesaquino@gmail.com>
2022-03-31 00:32:26 -0300

    Deleting unused converter

---
+++ src/test/java/com/meli/wallet/service/WalletServiceTest.java
@@ -0,0 +1,12 @@
+package com.meli.wallet.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletServiceTest {
+
+    @Test
+    void save() {
+    }
+}
